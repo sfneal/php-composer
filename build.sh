f7b1a52f1dbb9fdf4193e677ecbb68a7879ccdde
mrstephenneal <stephen.neal14@gmail.com>
2019-07-09 15:33:20 -0400

    MAKE php-composer build and push scripts

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker build -t stephenneal/php-composer:v1 ./v1/
+docker build -t stephenneal/php-composer:v2 ./v2/
---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker push stephenneal/php-composer:v1
+docker push stephenneal/php-composer:v2
