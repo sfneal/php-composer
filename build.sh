87d52180e816e7d94ce3c8764c31f912da7990ee
mrstephenneal <stephen.neal14@gmail.com>
2019-11-05 13:35:42 -0500

    ADD absolute directory resolving to build and push shell scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-docker build -t stephenneal/php-composer:v1 ./v1/
-docker build -t stephenneal/php-composer:v2 ./v2/
-docker build -t stephenneal/php-composer:v3 ./v3/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build -t stephenneal/php-composer:v1 "${DIR}"/v1/
+docker build -t stephenneal/php-composer:v2 "${DIR}"/v2/
+docker build -t stephenneal/php-composer:v3 "${DIR}"/v3/
