561f1071ce867907210457a94485b25e3a7ec59f
mrstephenneal <stephen.neal14@gmail.com>
2019-12-27 09:43:19 -0500

    FIX build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t stephenneal/php-composer:7.3-v1 "${DIR}"/v1/
-docker build -t stephenneal/php-composer:7.3-v2 "${DIR}"/v2/
-docker build -t stephenneal/php-composer:7.3-v3 "${DIR}"/v3/
-docker build -t stephenneal/php-composer:7.4-v1 "${DIR}"/v3/
+docker build -t stephenneal/php-composer:7.3-v1 "${DIR}"/7.3-v1/
+docker build -t stephenneal/php-composer:7.3-v2 "${DIR}"/7.3-v2/
+docker build -t stephenneal/php-composer:7.3-v3 "${DIR}"/7.3-v3/
+docker build -t stephenneal/php-composer:7.4-v1 "${DIR}"/7.4-v1/
