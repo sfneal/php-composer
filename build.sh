387d666973ea951105546b1b1a48aed99264c907
mrstephenneal <stephen.neal14@gmail.com>
2019-12-27 09:38:49 -0500

    REFACTOR image versions to have php version prefix

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t stephenneal/php-composer:v1 "${DIR}"/v1/
-docker build -t stephenneal/php-composer:v2 "${DIR}"/v2/
-docker build -t stephenneal/php-composer:v3 "${DIR}"/v3/
+docker build -t stephenneal/php-composer:7.3-v1 "${DIR}"/v1/
+docker build -t stephenneal/php-composer:7.3-v2 "${DIR}"/v2/
+docker build -t stephenneal/php-composer:7.3-v3 "${DIR}"/v3/
--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sh "${DIR}"/build.sh
 
-docker push stephenneal/php-composer:v1
-docker push stephenneal/php-composer:v2
-docker push stephenneal/php-composer:v3
+docker push stephenneal/php-composer:7.3-v1
+docker push stephenneal/php-composer:7.3-v2
+docker push stephenneal/php-composer:7.3-v3
