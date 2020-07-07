6d08af3b376114ba1afb120640f0f478d1350fa3
mrstephenneal <stephen.neal14@gmail.com>
2020-07-07 14:05:46 -0400

    MAKE 7.4-v4 image that uses composer:2
    MAKE 8.0-rc-v1 image that uses PHP 8.0 base image.

--- push.sh
+++ push.sh
@@ -10,3 +10,5 @@ docker push stephenneal/php-composer:7.3-v3
 docker push stephenneal/php-composer:7.4-v1
 docker push stephenneal/php-composer:7.4-v2
 docker push stephenneal/php-composer:7.4-v3
+docker push stephenneal/php-composer:7.4-v4
+docker push stephenneal/php-composer:8.0-rc-v1
