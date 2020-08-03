99d6dcc9a5363cc21c6e32f92841aa5fb8baa7bd
mrstephenneal <stephen.neal14@gmail.com>
2020-08-03 13:53:27 -0400

    REFACTOR new imagemagic tags to use 'x.1' tag

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ docker build -t stephenneal/php-composer:7.4-v1 "${DIR}"/7.4-v1/
 docker build -t stephenneal/php-composer:7.4-v2 "${DIR}"/7.4-v2/
 docker build -t stephenneal/php-composer:7.4-v3 "${DIR}"/7.4-v3/
 docker build -t stephenneal/php-composer:7.4-v4 "${DIR}"/7.4-v4/
-docker build -t stephenneal/php-composer:7.4-v5 "${DIR}"/7.4-v5/
+docker build -t stephenneal/php-composer:7.4-v4.1 "${DIR}"/7.4-v4.1/
 docker build -t stephenneal/php-composer:8.0-rc-v1 "${DIR}"/8.0-rc-v1/
-docker build -t stephenneal/php-composer:8.0-rc-v2 "${DIR}"/8.0-rc-v2/
+docker build -t stephenneal/php-composer:8.0-rc-v1.1 "${DIR}"/8.0-rc-v1.1/
