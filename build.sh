a38fdbed096a72d92deabeae85bbdbef78d16e36
Stephen Neal <stephen.neal14@gmail.com>
2021-01-04 12:23:55 -0500

    OPTIMIZE Travis CI config by adding env matrix & param passing to build.sh script
    FIX build.sh to accept arguments.

--- build.sh
+++ build.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
+# Base directory containing source code
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t stephenneal/php-composer:7.3-v1 "${DIR}"/7.3-v1/
-docker build -t stephenneal/php-composer:7.3-v2 "${DIR}"/7.3-v2/
-docker build -t stephenneal/php-composer:7.3-v3 "${DIR}"/7.3-v3/
-docker build -t stephenneal/php-composer:7.4-v1 "${DIR}"/7.4-v1/
-docker build -t stephenneal/php-composer:7.4-v2 "${DIR}"/7.4-v2/
-docker build -t stephenneal/php-composer:7.4-v4 "${DIR}"/7.4-v4/
-docker build -t stephenneal/php-composer:7.4-v4.1 "${DIR}"/7.4-v4.1/
-docker build -t stephenneal/php-composer:7.4-v5 "${DIR}"/7.4-v5/
-docker build -t stephenneal/php-composer:8.0-rc-v1 "${DIR}"/8.0-rc-v1/
-docker build -t stephenneal/php-composer:8.0-rc-v1.1 "${DIR}"/8.0-rc-v1.1/
-docker build -t stephenneal/php-composer:8.0-rc-v2 "${DIR}"/8.0-rc-v2/
-docker build -t stephenneal/php-composer:8.0-v1 "${DIR}"/8.0-v1/
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
+
+# Check if the TAG variable is set
+if [ -z "$TAG" ]
+
+  # Only build one image
+  then
+    docker build -t stephenneal/php-composer:"${TAG}" "${DIR}"/"${TAG}"/
+
+  # Build all images
+  else
+    docker build -t stephenneal/php-composer:7.3-v1 "${DIR}"/7.3-v1/
+    docker build -t stephenneal/php-composer:7.3-v2 "${DIR}"/7.3-v2/
+    docker build -t stephenneal/php-composer:7.3-v3 "${DIR}"/7.3-v3/
+    docker build -t stephenneal/php-composer:7.4-v1 "${DIR}"/7.4-v1/
+    docker build -t stephenneal/php-composer:7.4-v2 "${DIR}"/7.4-v2/
+    docker build -t stephenneal/php-composer:7.4-v4 "${DIR}"/7.4-v4/
+    docker build -t stephenneal/php-composer:7.4-v4.1 "${DIR}"/7.4-v4.1/
+    docker build -t stephenneal/php-composer:7.4-v5 "${DIR}"/7.4-v5/
+    docker build -t stephenneal/php-composer:8.0-rc-v1 "${DIR}"/8.0-rc-v1/
+    docker build -t stephenneal/php-composer:8.0-rc-v1.1 "${DIR}"/8.0-rc-v1.1/
+    docker build -t stephenneal/php-composer:8.0-rc-v2 "${DIR}"/8.0-rc-v2/
+    docker build -t stephenneal/php-composer:8.0-v1 "${DIR}"/8.0-v1/
+fi
