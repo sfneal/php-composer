12c1ef66700e458c609c6993441c87f1c40c97bf
Stephen Neal <stephen.neal14@gmail.com>
2021-03-15 12:51:42 -0400

    ADD 'deploy' section to Travis CI config for pushing Docker images to Docker Hub

--- push.sh
+++ push.sh
@@ -2,17 +2,31 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sh "${DIR}"/build.sh
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
 
-docker push stephenneal/php-composer:7.3-v1
-docker push stephenneal/php-composer:7.3-v2
-docker push stephenneal/php-composer:7.3-v3
-docker push stephenneal/php-composer:7.4-v1
-docker push stephenneal/php-composer:7.4-v2
-docker push stephenneal/php-composer:7.4-v4
-docker push stephenneal/php-composer:7.4-v4.1
-docker push stephenneal/php-composer:7.4-v5
-docker push stephenneal/php-composer:8.0-v1
-docker push stephenneal/php-composer:rc-v1
-docker push stephenneal/php-composer:rc-v1.1
-docker push stephenneal/php-composer:rc-v2
+# Check if the TAG variable is set
+if [ "$TAG" != null ]
+
+  # Only build & push one image
+  then
+    sh "${DIR}"/build.sh "${TAG}"
+    docker push stephenneal/php-composer:"${TAG}"
+
+  # Build & push all images
+  else
+    sh "${DIR}"/build.sh
+
+    docker push stephenneal/php-composer:7.3-v1
+    docker push stephenneal/php-composer:7.3-v2
+    docker push stephenneal/php-composer:7.3-v3
+    docker push stephenneal/php-composer:7.4-v1
+    docker push stephenneal/php-composer:7.4-v2
+    docker push stephenneal/php-composer:7.4-v4
+    docker push stephenneal/php-composer:7.4-v4.1
+    docker push stephenneal/php-composer:7.4-v5
+    docker push stephenneal/php-composer:8.0-v1
+    docker push stephenneal/php-composer:rc-v1
+    docker push stephenneal/php-composer:rc-v1.1
+    docker push stephenneal/php-composer:rc-v2
+fi
