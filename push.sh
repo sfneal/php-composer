176e0d6571a41b63bfd117d6296f9dbb736db404
sfneal <stephen.neal14@gmail.com>
2024-03-26 22:24:46 -0400

    enable PHP 8.3 builds

--- push.sh
+++ push.sh
@@ -51,6 +51,6 @@ if [ "$TAG" != null ]
     docker push stephenneal/php-composer:8.1-rc-v2
     docker push stephenneal/php-composer:8.2-v1
     docker push stephenneal/php-composer:8.2-v2
-#    docker push stephenneal/php-composer:8.3-v1
-#    docker push stephenneal/php-composer:8.3-v2
+    docker push stephenneal/php-composer:8.3-v1
+    docker push stephenneal/php-composer:8.3-v2
 fi
